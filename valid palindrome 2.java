53850dd62fb51a6144160851322522a3f5a91442
SanjanaPericharla <81902360+SanjanaPericharla@users.noreply.github.com>
2021-06-10 12:19:56 +0530

    Create valid palindrome 2.java

---
+++ valid palindrome 2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while(left <= right){
+            if(s.charAt(left) != s.charAt(right)){
+                return isvalidPalindrome(s,left+1,right) || isvalidPalindrome(s,left,right-1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public boolean isvalidPalindrome(String s,int start,int end){
+        while(start<=end){
+            if(s.charAt(start) != s.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
+// time complexity : O(n)
+//space complexity : O(1)
